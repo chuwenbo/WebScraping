6cfa426f40a68cad1b94ba0489b2756dd8ba94ee
unknown <chuwenbo86@163.com>
2018-08-16 23:44:27 -0700

    init

---
+++ Scraping.FOREX.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scraping.FOREX.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
